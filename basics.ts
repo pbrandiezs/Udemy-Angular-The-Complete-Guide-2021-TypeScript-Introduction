8031bd94bd27fd4f2cbcda22bae9d70dace76d36
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-27 13:15:31 -0700

    feat: Lesson 456 Diving into Functions and Function Types

--- basics.ts
+++ basics.ts
@@ -46,3 +46,12 @@ let course: string | number = 'React - The Complete Guide';
 
 course = 12341;
 
+// Functions and types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function printoutput(value: any) {
+  console.log(value);
+}

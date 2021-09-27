3eb98c3b4b496f1a84e1018e7e3918eb61cbe77e
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-27 12:41:20 -0700

    feat: Lesson 452 Array and Object Types

--- basics.ts
+++ basics.ts
@@ -16,3 +16,27 @@ let isInstructor: boolean;
 
 isInstructor = true;
 
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Max',
+  age: 32
+};
+
+// person = {
+//   isEmployee: true
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];

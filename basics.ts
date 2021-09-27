8a30502551f9f304deb740c2321028d27429375e
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-27 14:01:37 -0700

    feat: Lesson 458 Classes and TypeScript

--- basics.ts
+++ basics.ts
@@ -2,6 +2,8 @@
 // More complex types: arrays, objects
 // Function types, parameters
 
+import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
+
 // Primitives
 
 let age: number;
@@ -69,3 +71,31 @@ const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
 const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd')
 
 // updatedArray[0].split('');
+
+class Student {
+  // firstName: string;
+  // lastName: string;
+  // age: number;
+  // private courses: string[];
+
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public age: number,
+    private courses: string[]
+  ) {}
+
+  enroll(courseName: string) {
+    this.courses.push(courseName);
+  }
+
+  listCourses() {
+    return this.courses.slice();
+  }
+}
+
+const student = new Student('Max', 'Schwarz', 32, ['Angular']);
+student.enroll('React');
+// student.listCourses(); => Angular, React
+
+// student.courses => Angular, React
